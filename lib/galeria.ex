979fc220260bf9dabd202adcb6affbb5e797e0e0
Francisco Ceruti <hello@fceruti.com>
2023-09-25 13:24:58 -0300

    Run `mix new buenavista-galeria --app galeria`

---
+++ lib/galeria.ex
@@ -0,0 +1,18 @@
+defmodule Galeria do
+  @moduledoc """
+  Documentation for `Galeria`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Galeria.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/galeria_test.exs
@@ -0,0 +1,8 @@
+defmodule GaleriaTest do
+  use ExUnit.Case
+  doctest Galeria
+
+  test "greets the world" do
+    assert Galeria.hello() == :world
+  end
+end
