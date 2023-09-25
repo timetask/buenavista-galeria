5b332764ff0aeaebbb4483c3847eabe7edd5f0cc
Francisco Ceruti <hello@fceruti.com>
2023-09-25 17:21:51 -0300

    Update formatter

--- .formatter.exs
+++ .formatter.exs
@@ -1,4 +1,12 @@
-# Used by "mix format"
+locals_without_parens = [
+  galeria: 2,
+  galeria: 1
+]
+
 [
-  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"]
+  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
+  plugins: [BuenaVista.CssFormatter],
+  import_deps: [:phoenix, :buenavista],
+  locals_without_parens: locals_without_parens,
+  export: [locals_without_parens: locals_without_parens]
 ]
