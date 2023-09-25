a417fff0014b146f9f6351c192518b570e6923f3
Francisco Ceruti <hello@fceruti.com>
2023-09-25 13:40:25 -0300

    Add :phoenix_live_view and :buenavista as deps

--- mix.exs
+++ mix.exs
@@ -21,8 +21,8 @@ defmodule Galeria.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:phoenix_live_view, "~> 0.18"},
+      {:buenavista, path: "../buenavista"}
     ]
   end
 end
