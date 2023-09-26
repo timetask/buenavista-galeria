ab0c79bb8d00bae32854ed966c8294c29b436de6
Francisco Ceruti <hello@fceruti.com>
2023-09-26 01:08:16 -0300

    Update formatter

--- .formatter.exs
+++ .formatter.exs
@@ -1,11 +1,11 @@
 locals_without_parens = [
-  galeria: 2,
-  galeria: 1
+  galeria_routes: 2,
+  galeria_routes: 1
 ]
 
 [
   inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
-  plugins: [BuenaVista.CssFormatter],
+  plugins: [Phoenix.LiveView.HTMLFormatter, BuenaVista.CssFormatter],
   import_deps: [:phoenix, :buenavista],
   locals_without_parens: locals_without_parens,
   export: [locals_without_parens: locals_without_parens]
