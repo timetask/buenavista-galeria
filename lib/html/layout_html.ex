9f282a26c20872c8f7b5c338aac08332e59f1fbc
Francisco Ceruti <hello@fceruti.com>
2023-09-26 01:08:07 -0300

    Add assets config

--- lib/html/layout_html.ex
+++ lib/html/layout_html.ex
@@ -5,4 +5,16 @@ defmodule Galeria.HTML.Layout do
     only: [get_csrf_token: 0]
 
   embed_templates("layout/*")
+
+  def galeria_css_url(conn) do
+    prefix = conn.private.phoenix_router.__galeria_url_prefix__()
+    Path.join(prefix, "/css/galeria.css") |> dbg()
+  end
+
+  def current_theme_url(conn) do
+    prefix = conn.private.phoenix_router.__galeria_url_prefix__()
+    dbg(prefix)
+    dbg(conn)
+    "TODO"
+  end
 end
--- lib/router.ex
+++ lib/router.ex
@@ -1,19 +1,28 @@
 defmodule Galeria.Router do
-  defmacro galeria(path, opts \\ []) do
-    quote bind_quoted: binding() do
+  defmacro galeria_routes(path) do
+    quote bind_quoted: [path: path] do
       scope path, alias: false, as: false do
-        import Phoenix.Router, only: [get: 4]
+        import Plug.Builder, only: [plug: 2]
         import Phoenix.LiveView.Router, only: [live: 3, live_session: 3]
+        import Phoenix.Router, only: [get: 3, pipe_through: 1, scope: 2, scope: 3]
+
+        pipe_through BuenaVista.CurrentThemePlug
 
         live_session :galeria, root_layout: {Galeria.HTML.Layout, :root} do
-          # LiveDashboard assets
-          get "/css-:md5", Galeria.AssetsController, :css, as: :galeria_asset
-          get "/js-:md5", Galeria.AssetsController, :js, as: :galeria_asset
-
-          # All helpers are public contracts and cannot be changed
           live "/", Galeria.GaleriaLive, :index
           live "/ensambles/:ensamble_name", Galeria.GaleriaLive, :ensamble
           live "/component/:component_name", Galeria.GaleriaLive, :component
+        end
+
+        scope "/static" do
+          get "/css/:path", Galeria.AssetsController, :css
+          get "/js/:path", Galeria.AssetsController, :js
+          get "/themes/:path", Galeria.AssetsController, :themes
+        end
+
+        unless Module.get_attribute(__MODULE__, :galeria_url_prefix) do
+          @galeria_url_prefix Phoenix.Router.scoped_path(__MODULE__, "/static")
+          def __galeria_url_prefix__, do: @galeria_url_prefix
         end
       end
     end
