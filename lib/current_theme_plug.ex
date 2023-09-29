084146227b4d7b767d9e20cfa585c4a3decd4b6c
Francisco Ceruti <hello@fceruti.com>
2023-09-29 17:58:24 -0300

    Store :galeria_theme_name in session

--- lib/current_theme_plug.ex
+++ lib/current_theme_plug.ex
@@ -2,24 +2,13 @@ defmodule Galeria.CurrentThemePlug do
   def init(opts), do: opts
 
   def call(conn, _opts) do
-    theme =
-      if theme_name = conn.query_params["theme"] do
-        case Galeria.Config.find_theme(theme_name) do
-          {:ok, theme} -> theme
-          {:error, :not_found} -> Galeria.Config.get_default_theme()
-        end
-      else
-        with theme_name when is_binary(theme_name) <- Plug.Conn.get_session(conn, :theme_name),
-             {:ok, theme} <- Galeria.Config.find_theme(theme_name) do
-          theme
-        else
-          _ ->
-            Galeria.Config.get_default_theme()
-        end
-      end
+    case Plug.Conn.get_session(conn, :galeria_theme_name) do
+      theme_name when is_binary(theme_name) ->
+        conn
 
-    conn
-    |> Plug.Conn.put_session(:theme_name, theme.name)
-    |> Plug.Conn.assign(:theme, theme)
+      _ ->
+        {:ok, theme} = Galeria.Config.get_default_theme()
+        Plug.Conn.put_session(conn, :galeria_theme_name, theme.name)
+    end
   end
 end
