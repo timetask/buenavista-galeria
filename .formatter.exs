0c1a47c5bff577f47f0e4a92641bd17fc5f3e7f3
Francisco Ceruti <hello@fceruti.com>
2023-10-04 00:13:46 -0300

    Move formatter rules to buenavista

--- .formatter.exs
+++ .formatter.exs
@@ -1,12 +1,5 @@
-locals_without_parens = [
-  galeria_routes: 2,
-  galeria_routes: 1
-]
-
 [
   inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
   plugins: [Phoenix.LiveView.HTMLFormatter, BuenaVista.CssFormatter],
-  import_deps: [:phoenix, :buenavista],
-  locals_without_parens: locals_without_parens,
-  export: [locals_without_parens: locals_without_parens]
+  import_deps: [:phoenix, :buenavista]
 ]
