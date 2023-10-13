35fdce29089917056a081d2d39542c8dcce7957a
Francisco Ceruti <hello@fceruti.com>
2023-10-13 20:54:07 -0300

    Update watcher.sh

--- scripts/watchers.sh
+++ scripts/watchers.sh
@@ -10,6 +10,7 @@ components_dir="${project_dir}/lib/components"
 esbuild_config="--bundle"
 
 pids=()
+child_themes=(light dark)
 
 # Listen to component changes
 $watcher -w $components_dir/ -e ex \
@@ -18,35 +19,31 @@ $watcher -w $components_dir/ -e ex \
 pids+=("$!")
 
 # Listen to base hydrator changes
-$watcher -p -w $themes_dir/base_hydrator.ex \
-  mix galeria.gen.config --theme light --theme dark &
-    
-# Listen to theme hydrators changes
-$watcher -p -w $themes_dir/light_hydrator.ex \
-  "mix galeria.gen.intenal.css --theme light;
-  esbuild $assets_dir/themes/light.css --outdir=$static_dir/themes $esbuild_config" \
+$watcher -p -w $themes_dir/base/buenavista_galeria_hydrator.ex \
+  "mix galeria.gen.internal.themes --theme light --theme dark" \
   &
 pids+=("$!")
 
-$watcher -p -w $themes_dir/dark_hydrator.ex \
-  "mix galeria.gen.internal.css --theme dark; 
-  esbuild $assets_dir/themes/dark.css --outdir=$static_dir/themes $esbuild_config" \
-  &
-pids+=("$!")
+# Listen to child theme hydrators changes
+for theme in "${child_themes[@]}"; 
+do
+  $watcher -p -w $themes_dir/$theme/buenavista_galeria_hydrator.ex \
+    "mix galeria.gen.internal.css --theme $theme;
+    esbuild $assets_dir/themes/$theme.css --outdir=$static_dir/themes $esbuild_config" \
+    &
+  pids+=("$!")
+done
 
 # Listen to css changes
 $watcher -p -w $assets_dir/css/ -e css \
-  esbuild $assets_dir/css/galeria.css --outdir=$static_dir/css $esbuild_config &
+  esbuild $assets_dir/css/galeria.css --outdir=$static_dir/css $esbuild_config \
+  &
 pids+=("$!")
 
 # Listen to js changes
 $watcher -w $assets_dir/js/ -e js \
-  esbuild $assets_dir/js/galeria.js --outdir=$static_dir/js $esbuild_config &
-pids+=("$!")
-
-# Listen to icon changes
-$watcher -w $assets_dir/svg/ -e svg \
-  cp -R $assets_dir/svg/ $static_dir/svg/ &
+  esbuild $assets_dir/js/galeria.js --outdir=$static_dir/js $esbuild_config \
+  &
 pids+=("$!")
 
 echo "Press Ctrl-C to stop all watchers"
